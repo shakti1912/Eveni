74b06ac6664f784060798705170e0f47b2691ac4
Shakti Singh Rathore <rathore.shakti1912@gmail.com>
2017-01-24 04:53:28 -0800

    Update brandController.php

--- controllers/brandController.php
+++ controllers/brandController.php
@@ -9,8 +9,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 </head>
 <body>
-<p style='font-size:25px; font-family:serif;'>Eveni is a cloud based event management and photo sharing application. User can create an event and invited his/her friends to the event. All the invited users can view the photos of the event and can also upload photos to the cloud so that other invited users can see them. Currently we are only available on the desktop but our Android and iOS apps are coming soon.  </p>
-
+	<p style='font-size:25px; font-family:serif;'></p>
 <script type="text/javascript">
 	//function space(){
 	for (var i = 0; i < 25; i++) {
